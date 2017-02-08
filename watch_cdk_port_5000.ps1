2fc23750f02cade8623e807208b7958ecc12282d
Don Schenck <Don Schenck>
2017-02-08 13:34:28 -0500

    Initial code upload.

---
+++ watch_cdk_port_5000.ps1
@@ -0,0 +1,11 @@
+﻿clear
+while ($true) {
+    try{
+        (New-Object System.Net.WebClient).DownloadString("http://10.1.2.2:5000") 
+        Write-Host ""
+        sleep 1
+    } Catch {
+        Write-Host "-- service not available --"
+        sleep 4
+    }
+    }
---
+++ watch_openshift_myapp.ps1
@@ -0,0 +1,12 @@
+﻿clear
+while ($true) {
+    try{
+        #(New-Object System.Net.WebClient).DownloadString("http://dotnethello-mydotnet.rhel-cdk.10.1.2.2.xip.io") 
+        (New-Object System.Net.WebClient).DownloadString("http://dotnethello-mydotnet.rhel-cdk.10.1.2.2.nip.io") 
+        Write-Host ""
+        sleep 1
+    } Catch {
+        Write-Host "-- service not available --"
+        sleep 4
+    }
+    }
